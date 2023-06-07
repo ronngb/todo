f2fce98206efd9bf17c892aeab14800fa4787e4d
ronngb <ronny.gabi@gmail.com>
2023-06-07 23:51:52 +0800

    feat: new component JournalWeek.vue

---
+++ src/app/components/JournalWeek.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="journal-week" class="container">
+    <div class="columns is-mobile">
+      <!-- <CalendarDay v-for="day in sharedState.seedData" :key="day.id" :day="day" /> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import { store } from '../store.js'
+// import CalendarDay from './CalendarDay.vue';
+
+export default {
+  name: 'JournalWeek',
+  data() {
+    return {
+      sharedState: store.state,
+    }
+  },
+  components: {},
+}
+</script>
+
+<style lang="scss" scoped>
+#journal-week {
+  margin-bottom: 50px;
+  .column {
+    padding: 0 0 0 0;
+  }
+}
+</style>
